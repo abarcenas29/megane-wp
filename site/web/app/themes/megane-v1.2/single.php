ef0fe620deb5d47426a87fad9e21d281d2a176e1
Aldrich Allen Barcenas <aldrich.barcenas@gmail.com>
2017-10-11 00:19:25 +0800

    added styles on the single post types

--- site/web/app/themes/megane-v1.2/single.php
+++ site/web/app/themes/megane-v1.2/single.php
@@ -14,7 +14,10 @@ $post = Timber::query_post();
 $context['post'] = $post;
 $context['excerpt'] = get_the_excerpt($post);
 $context['categories'] = array_map(function ($cat) {
-  return $cat->name;
+  return array(
+    'name' => $cat->category_nicename,
+    'link' => get_category_link($cat->term_id)
+  );
 }, get_the_category($post->ID));
 
 if ( post_password_required( $post->ID ) ) {
