36c30f5136aa64956588b3ab1a9b2f20fa4fdddd
Aldrich Allen Barcenas <aldrich.barcenas@gmail.com>
2018-05-26 18:49:49 +0800

    Add cookie pop-up notification

--- site/web/app/themes/megane-v1.2/src/main/header.js
+++ site/web/app/themes/megane-v1.2/src/main/header.js
@@ -1,3 +1,16 @@
 import './../styles/header.scss'
+import $ from 'jquery-extensions'
+
+const isCookieAgree = (localStorage.getItem('ML_cookie_accept')) ? localStorage.getItem('ML_cookie_accept') : false
+
+const $cookieDialog = $('.cookie-dialog')
+if (!isCookieAgree) {
+  $cookieDialog.find('button').click(function () {
+    localStorage.setItem('ML_cookie_accept', true)
+    $cookieDialog.remove()
+  })
+} else {
+  $cookieDialog.remove()
+}
 
 console.log('Header')
