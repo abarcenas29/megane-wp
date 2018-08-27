69eb28ce7c0b76014c2ae3846c67faeafe17bfa3
Aldrich Allen Barcenas <aldrich.barcenas@gmail.com>
2018-08-27 12:59:53 +0800

    Add gify on single page

--- site/web/app/themes/megane-v1.2/common/acf/single.php
+++ site/web/app/themes/megane-v1.2/common/acf/single.php
@@ -50,4 +50,84 @@ if( function_exists('acf_add_local_field_group') ):
     'description' => '',
   ));
 
+  // Gify Options
+
+  acf_add_local_field_group(array(
+    'key' => 'group_5b82193ecc069',
+    'title' => 'Gify Options',
+    'fields' => array(
+      array(
+        'key' => 'field_5b82244b7bfe4',
+        'label' => 'Gify Options',
+        'name' => 'gify_options',
+        'type' => 'group',
+        'instructions' => 'Options for Gify',
+        'required' => 0,
+        'conditional_logic' => 0,
+        'wrapper' => array(
+          'width' => '',
+          'class' => '',
+          'id' => '',
+        ),
+        'layout' => 'block',
+        'sub_fields' => array(
+          array(
+            'key' => 'field_5b82194e0be14',
+            'label' => 'Gify Url',
+            'name' => 'gify_url',
+            'type' => 'url',
+            'instructions' => 'Insert the Gify URL to animate. Please use the mp4 format to save space.',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+              'width' => '',
+              'class' => '',
+              'id' => '',
+            ),
+            'default_value' => '',
+            'placeholder' => 'mp4 format',
+          ),
+          array(
+            'key' => 'field_5b82246c7bfe6',
+            'label' => 'scale',
+            'name' => 'scale',
+            'type' => 'number',
+            'instructions' => 'Overscan the video if the video doesn\'t cover the entire section.',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+              'width' => '',
+              'class' => '',
+              'id' => '',
+            ),
+            'default_value' => '1.2',
+            'placeholder' => '',
+            'prepend' => '',
+            'append' => '',
+            'min' => '',
+            'max' => '',
+            'step' => '',
+          ),
+        ),
+      ),
+    ),
+    'location' => array(
+      array(
+        array(
+          'param' => 'post_type',
+          'operator' => '==',
+          'value' => 'post',
+        ),
+      ),
+    ),
+    'menu_order' => 0,
+    'position' => 'normal',
+    'style' => 'default',
+    'label_placement' => 'top',
+    'instruction_placement' => 'label',
+    'hide_on_screen' => '',
+    'active' => 1,
+    'description' => '',
+  ));
+
   endif;
